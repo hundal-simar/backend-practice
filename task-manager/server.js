131e03afa5e222c100ba31c1e531e45c73ef0b6c
Simarpreet Kaur <ksimarpreet908@gmail.com>
2026-06-11 16:39:36 +0530

    cors helmet hpp express rate limit added and tested

--- task-manager/server.js
+++ task-manager/server.js
@@ -6,6 +6,12 @@ const taskRoutes = require('./routes/taskRoute');
 const commentRoutes = require('./routes/commentRoute');
 const errorHandler = require('./middleware/errorHandler');
 const authRoutes = require('./routes/authRoutes');
+const helmet = require('helmet');
+const cors = require('cors');
+const rateLimit = require('express-rate-limit');
+const xss = require('xss-clean');
+const mongoSanitize = require('express-mongo-sanitize');
+const hpp = require('hpp');
 
 require('dotenv').config();
 
@@ -17,9 +23,25 @@ mongoose.connect('mongodb://localhost:27017/', {
 }).catch((error) => {
     console.error('Error connecting to MongoDB:', error);
 });
-
 const app = express();
+
+app.use(helmet());
+app.use(cors({
+    origin: 'http://localhost:3000' || process.env.CLIENT_URL,
+    credentials: true
+}))
+
+app.use('/api', rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100 // limit each IP to 100 requests per windowMs
+}));
+
+
 app.use(express.json());
+
+//app.use(xss());
+//app.use(mongoSanitize());
+app.use(hpp({whitelist: ['sort', 'fields']}));
 
 app.use('/users', userRoutes);
 app.use('/projects', projectRoutes);
